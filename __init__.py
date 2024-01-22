1f0b20d5d1819852664a166c821eefe87bd64230
Stun Muffin <48792613+stunmuffin@users.noreply.github.com>
2024-01-22 22:58:55 +0300

    Update __init__.py
    Added Blender 4.1.0 Shade Auto Smooth

--- __init__.py
+++ __init__.py
@@ -1,21 +1,17 @@
 # ##### BEGIN GPL LICENSE BLOCK #####
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
 #
-#  This program is free software; you can redistribute it and/or
-#  modify it under the terms of the GNU General Public License
-#  as published by the Free Software Foundation; either version 2
-#  of the License, or (at your option) any later version.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
 #
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#
-#  You should have received a copy of the GNU General Public License
-#  along with this program; if not, write to the Free Software Foundation,
-#  Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
-#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
 # ##### END GPL LICENSE BLOCK #####
-
 
 # ----------------------------------------------
 # Define Addon info
@@ -31,9 +27,8 @@ bl_info = {
     "description": "Free opensource Blender add-on to help with your models",
 }
 
-import random
-import sys
-import os
+# Import statements
+import bpy
 
 from . import AB_Asset_Browser
 from . import AB_Camera
@@ -50,14 +45,15 @@ from . import AB_Color_Picker
 from . import AB_Wall
 from . import AB_MaterialManagement
 
-
-
-
+# Check Blender version
+if bpy.app.version >= (4, 1, 0):
+    from . import AB_Shade_Auto_Smooth
 
 # ------------------------------------------------------------------------------
 # REGISTRATION AREA
 
 def register():
+    # Register all modules
     AB_Asset_Browser.register()
     AB_Camera.register()
     AB_Create.register()
@@ -73,9 +69,12 @@ def register():
     AB_Wall.register()
     AB_MaterialManagement.register()
 
-
+    # Register Shade Auto Smooth if Blender version is compatible
+    if bpy.app.version >= (4, 1, 0):
+        AB_Shade_Auto_Smooth.register()
 
 def unregister():
+    # Unregister all modules
     AB_Asset_Browser.unregister()
     AB_Camera.unregister()
     AB_Create.unregister()
@@ -91,6 +90,9 @@ def unregister():
     AB_Wall.unregister()
     AB_MaterialManagement.unregister()
 
+    # Unregister Shade Auto Smooth if Blender version is compatible
+    if bpy.app.version >= (4, 1, 0):
+        AB_Shade_Auto_Smooth.unregister()
 
 # ------------------------------------------------------------------------------
 
